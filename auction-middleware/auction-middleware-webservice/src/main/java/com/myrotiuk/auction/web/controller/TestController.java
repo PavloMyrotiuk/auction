f46854377c1dda2e090dbeb9a563b224711bb701
Pavlo Myrotiuk <Myrotiuk.Pavlo@gmail.com>
2015-01-25 18:56:48 +0200

    deleted obsolete lines

--- auction-middleware/auction-middleware-webservice/src/main/java/com/myrotiuk/auction/web/controller/TestController.java
+++ auction-middleware/auction-middleware-webservice/src/main/java/com/myrotiuk/auction/web/controller/TestController.java
@@ -2,7 +2,6 @@ package com.myrotiuk.auction.web.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @RequestMapping(value ="/", method = RequestMethod.GET)
-    @ResponseBody
     public String test(){
         return "Hello";
     }
