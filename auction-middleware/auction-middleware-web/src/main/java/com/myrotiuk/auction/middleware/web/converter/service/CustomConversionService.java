6dc61d5eef9e2ec926c37da8b55d64f4ba65a5a9
Pavlo Myrotiuk <Myrotiuk.Pavlo@gmail.com>
2015-02-27 09:29:22 +0200

    small refactoring

--- auction-middleware/auction-middleware-web/src/main/java/com/myrotiuk/auction/middleware/web/converter/service/CustomConversionService.java
+++ auction-middleware/auction-middleware-web/src/main/java/com/myrotiuk/auction/middleware/web/converter/service/CustomConversionService.java
@@ -12,8 +12,10 @@ import java.util.List;
 public class CustomConversionService extends DefaultConversionService {
 
     public <T> List<T> convertAll(List<?> elems, Class<T> targetType){
-        if (elems == null) return null;
+
         Assert.notNull(targetType, "The targetType to convert to cannot be null");
+        if (elems == null) return null;
+
         List<T> result = new ArrayList<>();
         for (Object elem : elems){
             result.add(convert(elem, targetType));
